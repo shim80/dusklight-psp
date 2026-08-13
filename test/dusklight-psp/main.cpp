3df3b0a79b9b5910cf0e7f3f6d3eeb4985619632
shim80 <22941009+shim80@users.noreply.github.com>
2026-08-13 09:08:06 +0300

    Add canonical PSP launcher bootstrap

---
+++ test/dusklight-psp/main.cpp
@@ -0,0 +1,13 @@
+#include "dusk/psp/canonical_game.hpp"
+
+#include <pspkernel.h>
+
+PSP_MODULE_INFO("DusklightPSP", 0, 1, 0);
+PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER);
+PSP_HEAP_SIZE_KB(-256);
+
+int main() {
+    const int result = dusk::psp::game::run_canonical_game();
+    sceKernelExitGame();
+    return result;
+}
